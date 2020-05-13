90793ff0ee4de90ec7bc549655658c7582fd95ab
LinusBF <contact@linusbf.com>
2020-05-13 21:01:20 +0200

    Finished tasks

--- Hiddec.java
+++ Hiddec.java
@@ -4,9 +4,8 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.math.BigInteger;
 import java.security.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,24 +20,36 @@ public class Hiddec {
         }
         String hexKey = params.get("key");
         String ctrIV = params.get("ctr");
+        String outputFile = params.get("output");
         byte[] inputBytes = loadFile(params.get("input"));
-        byte[] decipheredBytes = ctrIV != null ? decryptByteArr(inputBytes, hexKey, ctrIV) : decryptByteArr(inputBytes, hexKey);
-        ArrayList<Integer> blockBoundaries = findHashedKeyBoundaries(decipheredBytes, hexKey);
-        byte[] payload = extractPayload(decipheredBytes, blockBoundaries.get(0), blockBoundaries.get(1), hexKey);
-        boolean verifiedPayload = verifyPayload(decipheredBytes, payload, blockBoundaries.get(1), hexKey);
-        System.out.println(verifiedPayload ? "Payload verified to be \"" + new String(payload) + "\"" : "Payload could not be verified");
+        byte[] deciphered = ctrIV != null ? findHiddenBlob(inputBytes, hexKey, ctrIV) : findHiddenBlob(inputBytes, hexKey);
+        if(deciphered == null) {
+            System.out.println("Couldn't find hidden blob!");return;
+        }
+        ArrayList<Integer> blockBoundaries = findHashedKeyBoundaries(deciphered, hexKey);
+        byte[] payload = extractPayload(deciphered, blockBoundaries.get(0), blockBoundaries.get(1), hexKey);
+        boolean verifiedPayload = verifyPayload(deciphered, payload, blockBoundaries.get(1), hexKey);
+        System.out.println(verifiedPayload ? new String(payload) : "Payload could not be verified");
+        if(outputFile != null){
+            writeToFile(payload, outputFile);
+        }
     }
 
-
-
     static byte[] loadFile(String fileName) throws IOException {
-        if(!(new File(fileName).isFile() && new File(fileName).canRead())){ throw new IllegalArgumentException("Dictionary needs to be readable!");}
+        if(!(new File(fileName).isFile() && new File(fileName).canRead())){ throw new IllegalArgumentException("File needs to be readable!");}
         File inputFile =  new File(fileName);
         FileInputStream inputStream = new FileInputStream(inputFile);
         byte[] inputBytes = new byte[(int)inputFile.length()];
         inputStream.read(inputBytes);
         inputStream.close();
         return inputBytes;
+    }
+
+    static void writeToFile(byte[] output, String fileName) throws IOException {
+        File outputFile = new File(fileName);
+        FileOutputStream writer = new FileOutputStream(outputFile);
+        writer.write(output);
+        writer.close();
     }
 
     static byte[] decryptByteArr(byte[] input, String hexKey) throws BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
@@ -48,15 +59,63 @@ public class Hiddec {
         return cipher.doFinal(input);
     }
 
-    static byte[] decryptByteArr(byte[] input, String hexKey, String hexIV) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    static byte[] decryptByteArr(byte[] input, String hexKey, byte[] hexIV) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         Key key = new SecretKeySpec(hexStringToByteArray(hexKey),"AES");
-        IvParameterSpec ivSpec = new IvParameterSpec(hexStringToByteArray(hexIV));
+        IvParameterSpec ivSpec = new IvParameterSpec(hexIV);
         Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
         cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
         return cipher.doFinal(input);
     }
 
-    static ArrayList<Integer> findHashedKeyBoundaries(byte[] deciphered, String hexKey) throws NoSuchAlgorithmException {
+    static byte[] findHiddenBlob(byte[] input, String hexKey) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException {
+        byte[] hashedKey = getHashedKey(hexKey);
+        int i = 0;
+        while (i < input.length - hashedKey.length) {
+            byte[] deciphered = decryptByteArr(sliceByteArr(input, i, i + 16), hexKey);
+            if(byteArrEq(hashedKey, deciphered)){
+                return decryptByteArr(sliceByteArr(input, i, input.length), hexKey);
+            }
+            i += 16;
+        }
+        return null;
+    }
+
+    static byte[] findHiddenBlob(byte[] input, String hexKey, String hexIV) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IOException {
+        byte[] hashedKey = getHashedKey(hexKey);
+        byte[] iv = hexStringToByteArray(hexIV);
+        int i = 0;
+        while (i < input.length - hashedKey.length) {
+            byte[] deciphered = decryptByteArr(sliceByteArr(input, i, i + 16), hexKey, iv);
+            if(byteArrEq(hashedKey, deciphered)){
+                return extractBlobFromIndex(input, i, hexKey, hexIV);
+            }
+            i += 16;
+        }
+        return null;
+    }
+
+    static byte[] extractBlobFromIndex(byte[] input, int blobStart, String hexKey, String hexIV) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IOException {
+        byte[] hashedKey = getHashedKey(hexKey);
+        byte[] iv = hexStringToByteArray(hexIV);
+        byte[] blob = decryptByteArr(sliceByteArr(input, blobStart, blobStart + 16), hexKey, iv);
+        int i = blobStart + 16;
+        while (i < input.length - hashedKey.length) {
+            byte[] deciphered = decryptByteArr(sliceByteArr(input, i, i + 16), hexKey, addToByteArray(iv, (i - blobStart) / 16));
+            if(byteArrEq(hashedKey, deciphered)){
+                blob = concatByteArr(blob, deciphered);
+                for(int j = 1; j <= 8; j++){
+                    blob = concatByteArr(blob, decryptByteArr(sliceByteArr(input, i + j*16, i + j*16 + 16), hexKey, addToByteArray(iv, ((i - blobStart) / 16) + j)));
+                }
+                break;
+            } else {
+                blob = concatByteArr(blob, deciphered);
+            }
+            i += 16;
+        }
+        return blob;
+    }
+
+    static ArrayList<Integer> findHashedKeyBoundaries(byte[] deciphered, String hexKey) throws NoSuchAlgorithmException, IllegalBlockSizeException {
         byte[] hashedKey = getHashedKey(hexKey);
         ArrayList<Integer> hashIndexes = new ArrayList<>();
         int i = 0;
@@ -97,6 +156,14 @@ public class Hiddec {
         return data;
     }
 
+    static byte[] addToByteArray(byte[] arr, int toAdd) throws IOException {
+        int bytes = new BigInteger(arr).intValue();
+        int n = bytes + toAdd;
+        byte[] a = sliceByteArr(arr, 0, arr.length - 4);
+        byte[] b = BigInteger.valueOf(n).toByteArray();
+        return concatByteArr(a, b);
+    }
+
     private static byte[] sliceByteArr(byte[] a, int start, int end){
         byte[] temp = new byte[end - start];
         for(int i = start; i < end; i++) {
@@ -116,6 +183,13 @@ public class Hiddec {
             }
         }
         return equal;
+    }
+
+    private static byte[] concatByteArr(byte[] a, byte[] b) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+        outputStream.write(a);
+        outputStream.write(b);
+        return outputStream.toByteArray();
     }
 
     // Inspired by https://stackoverflow.com/questions/7341683/parsing-arguments-to-a-java-command-line-program
--- Hidenc.java
+++ Hidenc.java
@@ -5,6 +5,7 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.math.BigInteger;
 import java.security.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,17 +20,17 @@ public class Hidenc {
         String hexKey = params.get("key");
         Integer offset = Integer.parseInt(params.get("offset"));
         String ctrIV = params.get("ctr");
+        String templateFile = params.get("template");
         byte[] payload = loadFile(params.get("input"));
         byte[] block = createBlock(payload, hexKey);
-        byte[] container = getContainer(Integer.parseInt(params.get("size")));
-        insertBlockIntoContainer(container, block, offset);
-        byte[] encryptedBytes = ctrIV != null ? encryptByteArr(container, hexKey, ctrIV) : encryptByteArr(container, hexKey);
-        writeToFile(encryptedBytes, params.get("output"));
-        System.out.println(encryptedBytes.length + " bytes written");
+        byte[] encryptedBytes = ctrIV != null ? encryptByteArr(block, hexKey, ctrIV) : encryptByteArr(block, hexKey);
+        byte[] container = templateFile != null ? loadFile(templateFile) : getContainer(Integer.parseInt(params.get("size")));
+        insertBlockIntoContainer(container, encryptedBytes, offset);
+        writeToFile(container, params.get("output"));
     }
 
     static byte[] loadFile(String fileName) throws IOException {
-        if(!(new File(fileName).isFile() && new File(fileName).canRead())){ throw new IllegalArgumentException("Dictionary needs to be readable!");}
+        if(!(new File(fileName).isFile() && new File(fileName).canRead())){ throw new IllegalArgumentException("File needs to be readable!");}
         File inputFile =  new File(fileName);
         FileInputStream inputStream = new FileInputStream(inputFile);
         byte[] inputBytes = new byte[(int)inputFile.length()];
@@ -68,12 +69,19 @@ public class Hidenc {
         return cipher.doFinal(input);
     }
 
-    static byte[] encryptByteArr(byte[] input, String hexKey, String hexIV) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    static byte[] encryptByteArr(byte[] input, String hexKey, String hexIV) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
         Key key = new SecretKeySpec(hexStringToByteArray(hexKey),"AES");
-        IvParameterSpec ivSpec = new IvParameterSpec(hexStringToByteArray(hexIV));
         Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-        cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
-        return cipher.doFinal(input);
+        byte[] encrypted = new byte[0];
+        int i = 0;
+        while(i < input.length) {
+            byte[] blockToEncrypt = sliceByteArr(input, i, i + 16);
+            IvParameterSpec ivSpec = new IvParameterSpec(addToByteArray(hexStringToByteArray(hexIV), i / 16));
+            cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+            encrypted = concatByteArr(encrypted, cipher.doFinal(blockToEncrypt));
+            i += 16;
+        }
+        return encrypted;
     }
 
     static void writeToFile(byte[] output, String fileName) throws IOException {
@@ -107,6 +115,29 @@ public class Hidenc {
         for(int i = 0; i < src.length; i++) {
             dest[i + start] = src[i];
         }
+    }
+
+    static byte[] addToByteArray(byte[] arr, int toAdd) throws IOException {
+        int bytes = new BigInteger(arr).intValue();
+        int n = bytes + toAdd;
+        byte[] a = sliceByteArr(arr, 0, arr.length - 4);
+        byte[] b = BigInteger.valueOf(n).toByteArray();
+        return concatByteArr(a, b);
+    }
+
+    private static byte[] sliceByteArr(byte[] a, int start, int end){
+        byte[] temp = new byte[end - start];
+        for(int i = start; i < end; i++) {
+            temp[i - start] = a[i];
+        }
+        return temp;
+    }
+
+    private static byte[] concatByteArr(byte[] a, byte[] b) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+        outputStream.write(a);
+        outputStream.write(b);
+        return outputStream.toByteArray();
     }
 
     // Inspired by https://stackoverflow.com/questions/7341683/parsing-arguments-to-a-java-command-line-program
